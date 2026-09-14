211628efa94c01c215fdbb77574bb2c95968d1e9
Chigui-27 <dchiguichon-2026187@kinal.edu.gt>
2026-09-14 11:51:15 -0600

    feat: se añaden interzafes graficas

--- src/org/perfectsmiles/system/controller/DashboardController.java
+++ src/org/perfectsmiles/system/controller/DashboardController.java
@@ -2,12 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package org.perfectsmiles.system.view;
+package org.perfectsmiles.system.controller;
 
 /**
  *
  * @author informatica
  */
-public class random9 {
+public class DashboardController {
     
 }
