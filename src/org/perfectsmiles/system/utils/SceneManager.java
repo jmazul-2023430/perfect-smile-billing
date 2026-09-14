5797e1f5423dc66aee7c5bd9d374b61f955f2e6d
Jose Rosales <jrosales-2023130@kinal.edu.gt>
2026-09-13 18:21:46 -0600

    feat: implementacion de scene manager y viewfactory

---
+++ src/org/perfectsmiles/system/utils/SceneManager.java
@@ -0,0 +1,48 @@
+package org.perfectsmiles.system.utils;
+
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class SceneManager {
+
+    private static SceneManager instanciaSceneManager;
+    private Stage stagePrincipal;
+
+    private SceneManager() {
+
+    }
+
+    public static SceneManager getInstanciaSceneManager() {
+        if (instanciaSceneManager == null) {
+            instanciaSceneManager = new SceneManager();
+        }
+        return instanciaSceneManager;
+    }
+
+    /**
+     * Este metodo permite recibir un parametro de tipo Scene y es la escena que
+     * se mostrara en el Stage
+     *
+     * @param scene Escena a mostrar en el Stage
+     */
+    public void changeScene(Scene scene) {
+        try {
+            stagePrincipal.setScene(scene);
+            stagePrincipal.sizeToScene();
+            stagePrincipal.show();
+
+        } catch (NullPointerException objectNull) {
+
+        }
+
+    }
+
+    public Stage getStagePrincipal() {
+        return stagePrincipal;
+    }
+
+    public void setStagePrincipal(Stage stagePrincipal) {
+        this.stagePrincipal = stagePrincipal;
+    }
+
+}
